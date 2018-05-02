1a728a3aa8c9117658cbbc764f5ef841341602a0
Ruan Reis <ruanvictorreis@gmail.com>
2018-05-02 15:34:08 -0300

    calling CLARA from server

--- python_modules/clara/clara_run.py
+++ python_modules/clara/clara_run.py
@@ -1,5 +1,32 @@
 import os
 
-os.system("dir >> output.txt")
+clara_path = 'python_modules/clara'
+
+# from args
+student_id = '111210442'
+
+# from args
+assignment = 'sum_of_squares_base'
+
+# from args
+parameters = '[[5, 5],[10,4],[0,3]]'
+
+# from args
+feedtype = 'code-repair'
+
+
+specs = '{0}/specs/{1}/*.py'.format(clara_path, assignment)
+
+file_name = student_id + '_' + assignment
+
+input_file = '{0}/attempts/{1}/{2}.py'.format(clara_path, assignment, file_name)
+
+output_file = '{0}/repairs/{1}/{2}.py'.format(clara_path, assignment, file_name)
+
+template = 'clara feedback {0} {1} --entryfnc {2} --args "{3}" --verbose 0 --feedtype "{4}"'
+
+command = template.format(specs, input_file, assignment, parameters, feedtype)
+
+os.system("{0} >> {1}".format(command, output_file))
 
 
--- routes/clara-service.js
+++ routes/clara-service.js
@@ -6,10 +6,11 @@ var PythonShell = require('python-shell')
 router.get('/', function (request, response) {
     PythonShell.run('python_modules/clara/clara_run.py', { args: [] }, (err) => {
         if (err) throw err
+        const content = fs.readFileSync(`./factorial.py`, 'utf8')
         //const content = fs.readFileSync(`./attempts/generated/${file}.json`, 'utf8')
         //response.json(content);
+        response.send(content);
     })
-    response.send("Hello world!!");
 });
 
 module.exports = router;
