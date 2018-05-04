30579f3a8794de3d0bc14d2fd97d7abee2e3bd4d
Ruan Reis <ruanvictorreis@gmail.com>
2018-05-04 17:12:03 -0300

    updating

--- routes/clara-service.js
+++ routes/clara-service.js
@@ -4,21 +4,21 @@ var router = express.Router();
 var PythonShell = require('python-shell')
 
 router.get('/', function (request, response) {
-    feedtype = 'code-repair'
-    student_id = '111210442'
-    assignment = 'sum_of_squares_base'
-    parameters = '[[5, 5],[10,4],[0,3]]'
-    student_attempt = '## SOME CODE HERE ##'
+  feedtype = 'code-repair'
+  student_id = '111210442'
+  assignment = 'sum_of_squares_base'
+  parameters = '[[5, 5],[10,4],[0,3]]'
+  student_attempt = '## SOME CODE HERE ##'
 
-    args = [feedtype, student_id, assignment, parameters, student_attempt]
+  args = [feedtype, student_id, assignment, parameters, student_attempt]
 
-    PythonShell.run('./python_modules/clara/clara_run.py', { args: args}, (err) => {
-        if (err) throw err
-        //const content = fs.readFileSync(`./factorial.py`, 'utf8')
-        //const content = fs.readFileSync(`./attempts/generated/${file}.json`, 'utf8')
-        //response.json(content);
-        response.send("Oi");
-    })
+  PythonShell.run('./python_modules/clara/clara_run.py', { args: args }, (err) => {
+    if (err) throw err
+    file_name = student_id + '.py'
+    repair_path = `./python_modules/clara/repairs/${assignment}/${file_name}`
+    const content = fs.readFileSync(repair_path, 'utf8')
+    response.send(content);
+  })
 });
 
 module.exports = router;
