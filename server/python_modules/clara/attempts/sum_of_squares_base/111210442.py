20bca809325bf2e8587541a32eb3c44639068e24
Ruan Reis <ruanvictorreis@gmail.com>
2018-05-09 13:28:32 -0300

    realocating files

--- server/python_modules/clara/attempts/sum_of_squares_base/111210442.py
+++
@@ -1,5 +0,0 @@
-def sum_of_squares_base(base, n):
-    total = base
-    for i in range(n):
-        total = base + (i^2)
-    return(total)
--- server/python_modules/clara/repairs/sum_of_squares_base/111210442.py
+++
@@ -1,5 +0,0 @@
-def sum_of_squares_base(base, n):
-    total = base
-    for i in range((n + 1)):
-        total = (total + (i ** 2))
-    return(total)
