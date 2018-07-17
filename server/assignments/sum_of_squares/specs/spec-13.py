22abdd8034eaf186ff8e030118e7c8723d91bc67
Ruan Reis <ruanvictorreis@gmail.com>
2018-07-16 22:40:47 -0300

    build

--- server/assignments/sum_of_squares/specs/spec-13.py
+++
@@ -1,4 +0,0 @@
-def sum_of_squares(n):
-  if n == 1:
-    return 1
-  return n**2 + sum_of_squares(n - 1)
--- server/assignments/sum_of_squares/specs/spec-14.py
+++
@@ -1,5 +0,0 @@
-def sum_of_squares(n):
-  if n == 1:
-    return 1
-  else:
-    return n**2 + sum_of_squares(n - 1)
