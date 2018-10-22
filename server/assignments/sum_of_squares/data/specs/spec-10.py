4276890982af86b934603e66b1572dcb9c2d226f
Ruan Reis <ruanvictorreis@gmail.com>
2018-10-22 19:24:10 -0300

    save progress

--- server/assignments/sum_of_squares/data/specs/spec-10.py
+++ server/assignments/sum_of_squares/data/specs/spec-10.py
@@ -1,6 +1,6 @@
-def sum_of_squares(n):
-  total = 0
-  number_list = range(1, n+1)
-  for i in number_list:
-    total = total + (i**2)
-  return total
+n = int(input())
+total = 0
+number_list = range(1, n+1)
+for i in number_list:
+  total = total + (i**2)
+print(total)
