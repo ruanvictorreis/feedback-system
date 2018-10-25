37c3257405d821249acec6b558ae9b12b83f9e00
Ruan Reis <ruanvictorreis@gmail.com>
2018-10-24 21:31:57 -0300

    training

--- server/assignments/fibonacci/data/training/training-29.py
+++ server/assignments/fibonacci/data/training/training-29.py
@@ -1,6 +1,6 @@
 def fibonacci(n):
-  lista = [0, 1, 1]
-  if(n > 2): 
-    for i in range(3, n+1): 
+  lista = [0, 1]
+  if(n > 1): 
+    for i in range(2, n+1): 
       lista.append(lista[i - 1] + lista[i - 2]) 
   return lista[n]
